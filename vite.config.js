b5fa2187eedffce0f0562bed38e632f4a851bc2a
Quach Dang Hoang My <quachdanghoangmy@192.168.1.8>
2026-08-30 16:09:14 +0700

    Update project 30/08/2026

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { resolve, dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-  plugins: [react()],
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        brand: resolve(__dirname, "brand.html"),
-      },
-    },
-  },
-});
+  plugins: [react(), tailwindcss()],
+})
